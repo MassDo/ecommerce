aae0ec1a0a5feda711f245e735eb99025f3e48fc
massdo <dorian_massoulier@hotmail.com>
2021-02-10 11:11:55 +0100

    starting product component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,16 +1,17 @@
 import products from "../products";
 import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
   return (
     <div>
       <h1>Latest products</h1>
       <Row>
-        {products.map((prod) => {
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{prod.name}</h3>
-          </Col>;
-        })}
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
       </Row>
     </div>
   );
