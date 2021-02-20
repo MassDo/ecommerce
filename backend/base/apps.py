72b38408052cba5de4caaa01ca98da3fd82d21f7
massdo <dorian_massoulier@hotmail.com>
2021-02-20 17:21:23 +0100

    login with email using signal

--- backend/base/apps.py
+++ backend/base/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class BaseConfig(AppConfig):
-    name = 'base'
+    # set email as username
+    user = instance
+    if user.email != '':
+        user.username = user.email
+
+    def ready(self):
+        import base.signals
