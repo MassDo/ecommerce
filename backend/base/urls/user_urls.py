413fcbc5d4bea681d77a50956126338b79fa4fb2
massdo <dorian_massoulier@hotmail.com>
2021-02-24 12:03:12 +0100

    Update profile endpoint: api PUT endpoint to modify name / email / password

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,8 +6,9 @@ from base.views import user_views as views
 urlpatterns=[
 
     path('', views.getUsers, name='users'),
-    path('profile/', views.getUserProfile, name='user-profile'),
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', views.getUserProfile, name='user-profile'),
+    path('profile/update/', views.updateUserProfile, name='user-profile-update'),
 
 ]
