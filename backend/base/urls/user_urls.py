f15c4add717b0f72c927b34bea33c57f5a4c9d1e
massdo <dorian_massoulier@hotmail.com>
2021-03-05 15:40:49 +0100

    New api endpoint for admin user: get user by ID update user by id

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,12 +4,15 @@ from base.views import user_views as views
 # extends url: api/users/
 
 urlpatterns=[
-
-    path('', views.getUsers, name='users'),
-    path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
+    # user url
     path('register/', views.registerUser, name='register'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name='user-profile'),
     path('profile/update/', views.updateUserProfile, name='user-profile-update'),
 
+    # admin url
+    path('', views.getUsers, name='users'),
+    path('<str:pk>/', views.getUserById, name='user'),
+    path('update/<str:pk>/', views.updateUser, name='user-update'),
+    path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 ]
