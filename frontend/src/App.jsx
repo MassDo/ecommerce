09c43a9b26b1e7250e1b829959bd1390f1427409
massdo <dorian_massoulier@hotmail.com>
2021-03-05 17:20:57 +0100

    admin update user screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
           <Route path="/order/:id" component={OrderScreen} exact />
 
           <Route path="/admin/userlist" component={UserListScreen} exact />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} exact />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/UserEditScreen.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import { getUserDetails } from "../actions/userActions";
+
+const EditUserScreen = ({ match, history }) => {
+  // Admin update component
+  const userId = match.params.id;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { error, loading, user } = userDetails;
+
+  useEffect(() => {
+    if (!user.name || user._id != number(userId)) {
+      dispatch(getUserDetails(userId));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [user, userId]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      {/* back to users list */}
+      <Link to="/admin/userlist">Go Back</Link>
+      {/*  Edit user */}
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger"> {error} </Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            {/* Name */}
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            {/* Email */}
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            {/* Is Admin */}
+            <Form.Group controlId="isadmin">
+              <Form.Check
+                type="checkbox"
+                label="Is Admin"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </div>
+  );
+};
+
+export default EditUserScreen;
